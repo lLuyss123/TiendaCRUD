5c98df0b062583170bf6471109b50f9485c26e36
Luis Mejia <lmejiaidep@gmail.com>
2026-05-16 12:46:14 -0500

    se cambio el guardar producto en la funcion del formulario cuando se envia en vez de tenerla al darle clic al boton de guardar

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,7 @@
 import "./styles/globals.css";
 import { getAll } from "./peticiones/peticionGet.js";
+import { postProduct } from "./peticiones/peticionPost.js";
+
+
 
 getAll();
--- client/src/peticiones/peticionPost.js
+++ client/src/peticiones/peticionPost.js
@@ -3,25 +3,6 @@ const formulario = document.getElementById("product-form");
 
 formulario.addEventListener("submit", (e) => {
   e.preventDefault();
-});
-export async function postProduct(data2) {
-  const respuesta = await fetch("http://localhost:8080/api/", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data2),
-  });
-
-  getAll();
-}
-
-const botonCrear = document.getElementById("btn-crear");
-
-const inputNombre = document.getElementById("nombre-producto");
-const inputPrecio = document.getElementById("precio-producto");
-const inputStock = document.getElementById("stock-producto");
-const inputDescrip = document.getElementById("descripcion-producto");
-
-const data2 = botonCrear.addEventListener("click", () => {
   postProduct({
     productName: `${inputNombre.value}`,
     productDescription: `${inputDescrip.value}`,
@@ -29,14 +10,39 @@ const data2 = botonCrear.addEventListener("click", () => {
     price: `${inputPrecio.value}`,
   });
 
+  notificacion.innerHTML = `<div class="bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3" role="alert">
+                  <p class="font-bold">Notificación</p>
+                  <p class="text-sm">Producto agregado correctamente.</p>
+                </div>`;
+
   limpiarForm();
 });
+export async function postProduct(data2) {
+  const respuesta = await fetch("http://localhost:8080/api/", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data2),
+  });
+  console.log(respuesta);
 
-const botonLimpiar = document.getElementById("btn-cancel");
-botonLimpiar.addEventListener("click", limpiarForm);
+  getAll();
+}
+const inputNombre = document.getElementById("nombre-producto");
+const inputPrecio = document.getElementById("precio-producto");
+const inputStock = document.getElementById("stock-producto");
+const inputDescrip = document.getElementById("descripcion-producto");
+const notificacion = document.getElementById("alert-notificacion");
+
 function limpiarForm() {
   inputNombre.value = "";
   inputPrecio.value = "";
   inputStock.value = "";
   inputDescrip.value = "";
+
+  setTimeout(() => {
+    notificacion.innerHTML = ``;
+  }, 3000);
 }
+
+const botonLimpiar = document.getElementById("btn-cancel");
+botonLimpiar.addEventListener("click", limpiarForm);
