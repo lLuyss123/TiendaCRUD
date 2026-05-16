199e564ea55e6e3b4206784fe323b04480a302ad
Luis Mejia <lmejiaidep@gmail.com>
2026-05-16 12:09:13 -0500

    cambios en la carpeta de peticiones, ahora cada peticion está deforma más independiente sin llamar tantas cosas en main

--- client/src/main.js
+++ client/src/main.js
@@ -1,22 +1,4 @@
 import "./styles/globals.css";
 import { getAll } from "./peticiones/peticionGet.js";
-import { postProduct } from "./peticiones/peticionPost.js";
 
-
-const botonCrear = document.getElementById("btn-crear");
-getAll()
-const data2 = botonCrear.addEventListener("click", () => {
-    
-  const inputNombre = document.getElementById("nombre-producto");
-  const inputPrecio = document.getElementById("precio-producto");
-  const inputStock = document.getElementById("stock-producto");
-  const inputDescrip = document.getElementById("descripcion-producto");
-
-    postProduct({
-    "productName": `${inputNombre.value}`,
-    "productDescription": `${inputDescrip.value}`,
-    "stock": `${inputStock.value}`,
-    "price": `${inputPrecio.value}`,
-  });
-  
-});
+getAll();
--- client/src/peticiones/peticionPost.js
+++ client/src/peticiones/peticionPost.js
@@ -5,14 +5,38 @@ formulario.addEventListener("submit", (e) => {
   e.preventDefault();
 });
 export async function postProduct(data2) {
-    
   const respuesta = await fetch("http://localhost:8080/api/", {
     method: "POST",
-    headers: { 'Content-Type': 'application/json' },
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data2),
   });
 
-  getAll()
+  getAll();
 }
 
+const botonCrear = document.getElementById("btn-crear");
 
+const inputNombre = document.getElementById("nombre-producto");
+const inputPrecio = document.getElementById("precio-producto");
+const inputStock = document.getElementById("stock-producto");
+const inputDescrip = document.getElementById("descripcion-producto");
+
+const data2 = botonCrear.addEventListener("click", () => {
+  postProduct({
+    productName: `${inputNombre.value}`,
+    productDescription: `${inputDescrip.value}`,
+    stock: `${inputStock.value}`,
+    price: `${inputPrecio.value}`,
+  });
+
+  limpiarForm();
+});
+
+const botonLimpiar = document.getElementById("btn-cancel");
+botonLimpiar.addEventListener("click", limpiarForm);
+function limpiarForm() {
+  inputNombre.value = "";
+  inputPrecio.value = "";
+  inputStock.value = "";
+  inputDescrip.value = "";
+}
